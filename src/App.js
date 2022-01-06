c57b24230c89afc7894a82d0a001eacf9de70827
itayasaf1990xd <89840492+itayasaf1990xd@users.noreply.github.com>
2022-01-06 02:38:57 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { HashRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import MemoryCard from "./pages/MemoryCard/MemoryCard.page";
+import TicTacToe from "./pages/TicTacToe/TicTacToe.page";
+import Home from "./pages/Home/Home.page";
+import Navbar from "./components/Navbar/Navbar.component";
 
 function App() {
   return (
     <>
       <HashRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/memorycard">
             <MemoryCard />
+          </Route>
+          <Route exact path="/tictactoe">
+            <TicTacToe />
           </Route>
           <Route path="/"></Route>
         </Switch>
